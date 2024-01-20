fa6ce6cdd1ebb661de766596660ab8305ae07e37
kevin prajapati <146225920+kevin5199@users.noreply.github.com>
2024-01-20 17:08:58 +0530

    Add files via upload

---
+++ git_repo/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int sum(int x,int y){
+  int sum=x+y;
+  return sum;
+  /*my name is kevin */
+}
+int main(){
+    int a,b;
+    printf("enter the value of a and b");
+    scanf("%d %d",&a,&b);
+    int Sum=sum(a,b);
+    printf("sum of two number %d",Sum);
+}
